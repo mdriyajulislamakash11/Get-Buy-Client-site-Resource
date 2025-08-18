51cca31a4039bc850c2ae23587c376fcd669550f
Akash <hafejmohammadakash@gmail.com>
2025-08-19 00:56:58 +0600

    call us

--- src/pages/Popular-Product/PopularProduct.jsx
+++ src/pages/Popular-Product/PopularProduct.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import useProduct from "../../hook/useProduct";
 import SectionlTitle from "../Share/SectionTitle";
+import ProductInfoCard from "../ProductInfoCard/ProductInfoCard";
 
 const PopularProduct = () => {
   const [product] = useProduct();
@@ -16,8 +17,24 @@ const PopularProduct = () => {
         subHeading="Check out our most popular items"
       />
 
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 ">
+        {popularProduct.map((item) => (
+          <ProductInfoCard key={item._id} item={item} />
+        ))}
+      </div>
 
-      
+      {/* button */}
+      <div className="flex justify-center my-3">
+        <button className="btn btn-outline">View All</button>
+      </div>
+
+      {/* number Contact */}
+        <div className="flex my-16 justify-center items-center bg-black w-full h-60">
+            <h2 className="text-4xl font-bold text-white">Call Us: 01307784859 GP</h2>
+        </div>
+
+
+        
     </div>
   );
 };
---
+++ src/pages/ProductInfoCard/ProductInfoCard.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const ProductInfoCard = ({ item }) => {
+  const { name, description, image, price } = item || {};
+
+  return (
+    <div>
+      <div className="flex space-x-2 border">
+        <img
+          style={{ borderRadius: "0 200px 200px 200px" }}
+          className="w-[100px]"
+          src={image}
+          alt=""
+        />
+        <div>
+          <h3 className="uppercase">{name}----------</h3>
+          <p>{description}</p>
+        </div>
+        <p className="text-yellow-500">${price}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProductInfoCard;
