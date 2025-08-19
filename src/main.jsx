28331869c95a33b5073a22e1bb868b03df15a5a4
Akash <hafejmohammadakash@gmail.com>
2025-08-19 14:19:25 +0600

    test,umiyal done

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-
-
-
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <div className="max-w-screen-2xl mx-auto">
-      <RouterProvider router={router} />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="max-w-screen-2xl mx-auto">
+        <RouterProvider router={router} />
+      </div>
+    </QueryClientProvider>
   </StrictMode>
 );
--- src/pages/TestMunial/TestMunial.jsx
+++ src/pages/TestMunial/TestMunial.jsx
@@ -1,23 +1,66 @@
-import React from "react";
-import SectionlTitle from "../Share/SectionTitle";
+import SectionTitle from "../Share/SectionTitle";
+import useAxiosPublic from "../../hook/useAxiosPublic";
+import { useQuery } from "@tanstack/react-query";
+import { Swiper, SwiperSlide } from "swiper/react";
 
-const TestMunial = () => {
+// rating
+import { Rating } from "@smastrom/react-rating";
+import "@smastrom/react-rating/style.css";
 
-    
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
 
+// import required modules
+import { Navigation } from "swiper/modules";
+import { useState } from "react";
+
+const Testimonial = () => {
+  const axiosPublic = useAxiosPublic();
+
+  const { data: reviews = [] } = useQuery({
+    queryKey: ["review"],
+    queryFn: async () => {
+      const res = await axiosPublic.get("/reviews");
+      return res.data;
+    },
+  });
 
   return (
-    <div>
-      <SectionlTitle
+    <>
+      <SectionTitle
         heading={"Testimonials"}
-        subHeading={"What Our Client Say"}
+        subHeading={"What Our Clients Say"}
       />
 
-
-
-
-    </div>
+      <Swiper
+        rewind={true}
+        navigation={true}
+        modules={[Navigation]}
+        className="mySwiper"
+      >
+        <div>
+          {reviews?.map((review) => (
+            <SwiperSlide key={review._id}>
+              <div className="border p-4 my-2 text-center ">
+                <div className="flex justify-center my-10">
+                  <p className="">
+                    <Rating
+                      style={{ maxWidth: 250 }}
+                      value={review.rating}
+                      readOnly={true}
+                    />
+                  </p>
+                </div>
+                <h2 className="text-4xl font-semibold">{review.name}</h2>
+                <p className="my-3">{review.details}</p>
+              </div>
+            </SwiperSlide>
+          ))}
+        </div>
+      </Swiper>
+    </>
   );
 };
 
-export default TestMunial;
+export default Testimonial;
